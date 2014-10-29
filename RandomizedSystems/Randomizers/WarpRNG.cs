3bfa2a14de0f9e588efc991f592a7a7e45ab0f94
pulli23 <pweijtens@gmail.com>
2014-10-29 11:40:03 +0100

    Changed warpRNG to better fullfill RNG reqs
    Reseeding a random number each operation is worse - leeding to several
    times the same sequence. Also uses string.gethashcode now to actually
    get a good hash from a string (as a consequence you could type the solar
    system by name now).

--- RandomizedSystems/Randomizers/WarpRNG.cs
+++ RandomizedSystems/Randomizers/WarpRNG.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using System;
 
 namespace RandomizedSystems.Randomizers
 {
-	/// <summary>
-	/// Generates pseudo-random values based off of a seed.
-	/// </summary>
-	public static class WarpRNG
-	{
-		/// <summary>
-		/// Different prefixes for star names.
-		/// </summary>
-		public static string[] prefixes = new string[] {
+    /// <summary>
+    /// Generates pseudo-random values based off of a seed.
+    /// </summary>
+    public static class WarpRNG
+    {
+        /// <summary>
+        /// Different prefixes for star names.
+        /// </summary>
+        public static string[] prefixes = new string[] {
 			"Ker",
 			"Jo",
 			"Ear",
@@ -36,10 +37,10 @@ namespace RandomizedSystems.Randomizers
 			"S",
 			"Sat"
 		};
-		/// <summary>
-		/// Different suffixes for star names.
-		/// </summary>
-		public static string[] suffixes = new string[] {
+        /// <summary>
+        /// Different suffixes for star names.
+        /// </summary>
+        public static string[] suffixes = new string[] {
 			"bin",
 			"ol",
 			"th",
@@ -64,83 +65,112 @@ namespace RandomizedSystems.Randomizers
 			"pau",
 			"far",
 		};
-		private static int index = 0;
+        private static int index = 0;
+        private static long seed = 0;
+        private static System.Random rng = new System.Random(0);
 
-		/// <summary>
-		/// Generates an int between min (inclusive) and max (exclusive)
-		/// </summary>
-		/// <returns>A pseudorandom int.</returns>
-		/// <param name="min">Minimum.</param>
-		/// <param name="max">Max.</param>
-		public static int GenerateInt (int min, int max)
-		{
-			Seed ();
-			return Random.Range (min, max);
-		}
 
-		/// <summary>
-		/// Generates a float between min and max, inclusive.
-		/// </summary>
-		/// <returns>A pseudorandom float.</returns>
-		/// <param name="min">Minimum.</param>
-		/// <param name="max">Max.</param>
-		public static float GenerateFloat (float min, float max)
-		{
-			Seed ();
-			return Random.Range (min, max);
-		}
+        /// <summary>
+        /// Initializes the randomizer from a string
+        /// </summary>
+        /// <param name="seedstring">seedstring</param>
+        public static void ReSeed(string seedstring)
+        {
+            rng = new System.Random(seedstring.GetHashCode());
+        }
 
-		/// <summary>
-		/// Generates a float between 0 and 1, inclusive.
-		/// </summary>
-		/// <returns>A pseudorandom value.</returns>
-		public static float GetValue ()
-		{
-			Seed ();
-			return Random.value;
-		}
+        /// <summary>
+        /// Generates an int between min (inclusive) and max (exclusive)
+        /// </summary>
+        /// <returns>A pseudorandom int.</returns>
+        /// <param name="min">Minimum.</param>
+        /// <param name="max">Max.</param>
+        public static int GenerateInt(int min, int max)
+        {
+            return rng.Next(min, max);
+        }
 
-		/// <summary>
-		/// Generates a random name, composed of prefix + suffix.
-		/// </summary>
-		/// <returns>A name.</returns>
-		public static string GenerateName ()
-		{
-			Seed ();
-			string prefix = prefixes [Random.Range (0, prefixes.Length)];
-			string suffix = suffixes [Random.Range (0, suffixes.Length)];
-			return prefix + suffix;
-		}
+        /// <summary>
+        /// Generates a float between min and max, exclusive.
+        /// </summary>
+        /// <returns>A pseudorandom float.</returns>
+        /// <param name="min">Minimum.</param>
+        /// <param name="max">Max.</param>
+        public static float GenerateFloat(float min, float max)
+        {
+            return (float)GenerateDouble(min, max);
+        }
 
-		/// <summary>
-		/// Seeds the RNG.
-		/// </summary>
-		private static void Seed ()
-		{
-			if (string.IsNullOrEmpty (Hyperdrive.seedString))
-			{
-				// Can't seed
-				return;
-			}
-			// Reset the index if we exceed the string length
-			if (index >= Hyperdrive.seedString.Length)
-			{
-				index = 0;
-			}
-			// We convert the next character in the seed string to an int
-			int nextValue = (int)Hyperdrive.seedString [index];
-			if (int.MaxValue - nextValue <= Hyperdrive.seed)
-			{
-				// Very unlikely to ever happen, but better safe than sorry
-				Hyperdrive.seed = 0;
-			}
-			// Add the char's value to the seed
-			Hyperdrive.seed += nextValue;
-			// Seed the RNG
-			Random.seed = Hyperdrive.seed;
-			// Increment the index
-			index++;
-		}
-	}
+        /// <summary>
+        /// Generates a double between min and max, exclusive.
+        /// </summary>
+        /// <returns>A pseudorandom double.</returns>
+        /// <param name="min">Minimum.</param>
+        /// <param name="max">Max.</param>
+        public static double GenerateDouble(double min, double max)
+        {
+            return min + rng.NextDouble() * (max - min);
+        }
+
+
+        /// <summary>
+        /// Generates a float between 0 and 1, inclusive.
+        /// </summary>
+        /// <returns>A pseudorandom value.</returns>
+        public static float GetValue()
+        {
+            return (float)rng.NextDouble();
+        }
+
+        /// <summary>
+        /// Generates a double between 0 and 1, inclusive.
+        /// </summary>
+        /// <returns>A pseudorandom value.</returns>
+        public static double GetValueDouble()
+        {
+            return rng.NextDouble();
+        }
+
+        /// <summary>
+        /// Generates a random name, composed of prefix + suffix.
+        /// </summary>
+        /// <returns>A name.</returns>
+        public static string GenerateName()
+        {
+            string prefix = prefixes[rng.Next(prefixes.Length)];
+            string suffix = suffixes[rng.Next(suffixes.Length)];
+            return prefix + suffix;
+        }
+
+        /// <summary>
+        /// Seeds the RNG. - OBSOLETE!!!
+        /// </summary>
+        private static void Seed()
+        {
+            if (string.IsNullOrEmpty(Hyperdrive.seedString))
+            {
+                // Can't seed
+                return;
+            }
+            // Reset the index if we exceed the string length
+            if (index >= Hyperdrive.seedString.Length)
+            {
+                index = 0;
+            }
+            // We convert the next character in the seed string to an int
+            int nextValue = (int)Hyperdrive.seedString[index];
+            if (int.MaxValue - nextValue <= Hyperdrive.seed)
+            {
+                // Very unlikely to ever happen, but better safe than sorry
+                Hyperdrive.seed = 0;
+            }
+            // Add the char's value to the seed
+            Hyperdrive.seed += nextValue;
+            // Seed the RNG
+            UnityEngine.Random.seed = Hyperdrive.seed;
+            // Increment the index
+            index++;
+        }
+    }
 }
 
